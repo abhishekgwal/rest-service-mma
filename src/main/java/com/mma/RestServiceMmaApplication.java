19c02abea83daa749f78a0bde40ca71e222f767c
abhishekgwal <abhishekgwal@gmail.com>
2021-05-11 16:05:23 -0400

    Updated package name and removed annotations not getting used

---
+++ src/main/java/com/mma/RestServiceMmaApplication.java
@@ -0,0 +1,13 @@
+package com.mma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServiceMmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServiceMmaApplication.class, args);
+	}
+
+}
